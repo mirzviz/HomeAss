b4d90a37ab43def4fa9712c34d44d59f644f18bb
Zvi Mirimsky <mirzviz@gmail.com>
2019-11-29 14:43:29 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+const axios = require("axios");
 
 function App() {
+  const [country, setCountry] = useState("");
+  const [city, setCity] = useState("");
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axios
+      .get(
+        "https://api.ipdata.co?api-key=b0b8cd08bd4c1824cd4df64ab44f6975fc71e6ba25914603fe048c61"
+      )
+      .then(res => {
+        console.log(res.data.country_name, res.data.city);
+        setCountry(res.data.country_name);
+        setCity(res.data.city);
+      })
+      .catch(err => console.log(err));
+  };
+
+  let url = `https://www.google.com/maps/embed/v1/place?q=${country}+${city}&key=AIzaSyCdQymwSuF0P6Ee-ffX0ZtWjpJdpaT5eLk`;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {city && country && (
+        <>
+          <h3 className="div">Country: {country} ,City: {city}</h3>
+          <iframe width="600" height="450" src={url}></iframe>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/keys.js
@@ -0,0 +1,2 @@
+export const ipDataKey = 'b0b8cd08bd4c1824cd4df64ab44f6975fc71e6ba25914603fe048c61';
+export const GoogleMapsKey = 'AIzaSyCdQymwSuF0P6Ee-ffX0ZtWjpJdpaT5eLk';
