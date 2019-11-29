e4252f56da34cf8e299ab6b7f4cd3c08873964d8
Zvi Mirimsky <mirzviz@gmail.com>
2019-11-29 14:46:59 +0200

    fixed serius stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import {ipDataKey, GoogleMapsKey} from './keys';
 const axios = require("axios");
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
   const getData = () => {
     axios
       .get(
-        "https://api.ipdata.co?api-key=b0b8cd08bd4c1824cd4df64ab44f6975fc71e6ba25914603fe048c61"
+        `https://api.ipdata.co?api-key=${ipDataKey}`
       )
       .then(res => {
         console.log(res.data.country_name, res.data.city);
@@ -24,7 +25,7 @@ function App() {
       .catch(err => console.log(err));
   };
 
-  let url = `https://www.google.com/maps/embed/v1/place?q=${country}+${city}&key=AIzaSyCdQymwSuF0P6Ee-ffX0ZtWjpJdpaT5eLk`;
+  let url = `https://www.google.com/maps/embed/v1/place?q=${country}+${city}&key=${GoogleMapsKey}`;
   return (
     <div className="App">
       {city && country && (
