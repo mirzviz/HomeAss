a5140ddd0ff3b2ee4cfe21c8ee09837f186d575e
Zvi Mirimsky <mirzviz@gmail.com>
2019-11-29 14:53:49 +0200

    hgj

--- src/keys.js
+++ src/keys.js
@@ -1,2 +1,2 @@
-export const ipDataKey = 'b0b8cd08bd4c1824cd4df64ab44f6975fc71e6ba25914603fe048c61';
-export const GoogleMapsKey = 'AIzaSyCdQymwSuF0P6Ee-ffX0ZtWjpJdpaT5eLk';
+export const ipDataKey = 'b0b8cd08bd4c1824cd4df64ab44f6975fc71e6ba25914603fe048c61'; //ipDataKey key
+export const GoogleMapsKey = 'AIzaSyCdQymwSuF0P6Ee-ffX0ZtWjpJdpaT5eLk'; // GoogleMapsKey key
