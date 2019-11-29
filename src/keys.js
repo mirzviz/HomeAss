db7337537854c8418d8610bc4afd11c8c5d13fe8
Zvi Mirimsky <mirzviz@gmail.com>
2019-11-29 15:09:37 +0200

    secured

--- src/keys.js
+++
@@ -1,2 +0,0 @@
-export const ipDataKey = 'b0b8cd08bd4c1824cd4df64ab44f6975fc71e6ba25914603fe048c61'; //ipDataKey key
-export const GoogleMapsKey = 'AIzaSyCdQymwSuF0P6Ee-ffX0ZtWjpJdpaT5eLk'; // GoogleMapsKey keyyy
